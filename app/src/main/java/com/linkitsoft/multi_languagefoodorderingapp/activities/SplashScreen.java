30d33525217866379319dcc43f910e377edae9b4
Muhammad Mohsin <mohsin@linkitsoft.com>
2026-05-15 00:28:19 +0500

    working on drawables

--- app/src/main/java/com/linkitsoft/multi_languagefoodorderingapp/activities/SplashScreen.java
+++ app/src/main/java/com/linkitsoft/multi_languagefoodorderingapp/activities/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.linkitsoft.multi_languagefoodorderingapp.activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,15 +14,17 @@ import com.linkitsoft.multi_languagefoodorderingapp.R;
 
 public class SplashScreen extends AppCompatActivity {
 
+    Handler handler = new Handler();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        handler.postDelayed(() -> {
+            Intent intent = new Intent(SplashScreen.this, HomeScreen.class);
+            startActivity(intent);
+            finish();
+        },2000);
     }
 }
