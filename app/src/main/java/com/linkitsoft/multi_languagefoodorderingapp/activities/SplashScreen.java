30e57cbbdc67dea005e097a4a4e7c6e79f742a2e
Muhammad Mohsin <mohsin@linkitsoft.com>
2026-05-15 23:53:41 +0500

    completed button background and worked with states

--- app/src/main/java/com/linkitsoft/multi_languagefoodorderingapp/activities/SplashScreen.java
+++ app/src/main/java/com/linkitsoft/multi_languagefoodorderingapp/activities/SplashScreen.java
@@ -1,16 +1,38 @@
 package com.linkitsoft.multi_languagefoodorderingapp.activities;
 
+import android.app.AlarmManager;
+import android.app.Application;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+import android.text.TextUtils;
+import android.util.Log;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.linkitsoft.multi_languagefoodorderingapp.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Locale;
+import java.util.UUID;
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -21,10 +43,10 @@ public class SplashScreen extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_splash_screen);
-        handler.postDelayed(() -> {
+        /*handler.postDelayed(() -> {
             Intent intent = new Intent(SplashScreen.this, HomeScreen.class);
             startActivity(intent);
             finish();
-        },2000);
+        },2000);*/
     }
 }
