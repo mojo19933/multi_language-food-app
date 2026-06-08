bcedc97e418e5db1df46821f053863c8cc4a5f39
Muhammad Mohsin <mohsin@linkitsoft.com>
2026-06-09 00:04:52 +0500

    working on chips

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -8,6 +8,10 @@ android {
         version = release(36)
     }
 
+    buildFeatures {
+        viewBinding = true
+    }
+
     defaultConfig {
         applicationId = "com.linkitsoft.multi_languagefoodorderingapp"
         minSdk = 24
--- app/src/main/java/com/linkitsoft/multi_languagefoodorderingapp/activities/HomeScreen.java
+++ app/src/main/java/com/linkitsoft/multi_languagefoodorderingapp/activities/HomeScreen.java
@@ -1,6 +1,7 @@
 package com.linkitsoft.multi_languagefoodorderingapp.activities;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,18 +10,60 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 import com.linkitsoft.multi_languagefoodorderingapp.R;
+import com.linkitsoft.multi_languagefoodorderingapp.databinding.ActivityHomeScreenBinding;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class HomeScreen extends AppCompatActivity {
+
+    ActivityHomeScreenBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_home_screen);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+        binding = ActivityHomeScreenBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        /*ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });*/
+        initViews();
+        clickListener();
+    }
+
+    private void initViews() {
+        binding.chipPizzaText.setSelected(true);
+        binding.chipBurgerText.setSelected(false);
+        binding.chipDrinksText.setSelected(false);
+    }
+
+    private void clickListener() {
+        binding.chipPizzaText.setOnClickListener(view -> {
+            selectChip(binding.chipPizzaText);
+        });
+        binding.chipBurgerText.setOnClickListener(view -> {
+            selectChip(binding.chipBurgerText);
+        });
+        binding.chipDrinksText.setOnClickListener(view -> {
+            selectChip(binding.chipDrinksText);
         });
     }
+
+
+    List<TextView> chips = Arrays.asList(
+            binding.chipPizzaText,
+            binding.chipBurgerText,
+            binding.chipDrinksText
+    );
+
+    private void selectChip(TextView selectedChip) {
+        for (TextView chip : chips) {
+            chip.setSelected(false);
+        }
+        selectedChip.setSelected(true);
+    }
+
 }
